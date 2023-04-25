08078bbf9474e0430bb7b704e730506e6196642c
Luna <vultines@gmail.com>
2023-04-24 21:49:19 -0400

    Home Page Updated

--- centros_i_web/src/pages/index.tsx
+++ centros_i_web/src/pages/index.tsx
@@ -38,10 +38,44 @@ export default function Home() {
           <div className="photoSection justify-center items-center">
             <img src="/images/mainPic.png"></img>
           </div>
+          
         </div>
        
+    
+
       </div>
 
+      <div className="aboutMainContainer ">
+        <div className="aboutTitle">
+          Acerca de Nosotros
+        </div>
+
+        <div className="aboutSubContainer flex flex-row">
+          
+          <div className="aboutImgs1">
+            <img src="/images/kidsClg1.png"></img>
+          </div>
+          <div className="aboutDesc">
+            
+              Descripcion breve sobre la informacion y contenido de los centros infantiles de Cochabamba.
+              Esta pagina web ayudara a encontrar informacion acerca de cada cento infantil.
+              
+              <br></br>
+              
+              En los centros infantiles, los niños son atendidos por profesionales capacitados en la atención infantil y educación temprana. 
+              Se les proporciona una variedad de actividades diseñadas para ayudarlos a aprender y desarrollarse, como juegos, canciones, actividades artísticas, entre otras. 
+              Además, se les proporciona una alimentación equilibrada y adecuada para su edad.
+
+              <br></br>
+              Los centros infantiles son establecimientos que ofrecen cuidado y educación a niños y niñas desde los primeros meses de vida hasta los seis años de edad. 
+              Estos centros tienen como objetivo brindar un entorno seguro y estimulante para el desarrollo físico, emocional, social y cognitivo de los niños.
+          </div>
+          <div className="aboutImgs1">
+            <img src="/images/kidsClg1.png"></img>
+          </div>
+        </div>
+      </div>
+      
 
     </div>
   )
--- centros_i_web/src/pages/map.tsx
+++ centros_i_web/src/pages/map.tsx
@@ -57,26 +57,150 @@ function MyComponent() {
                 onUnmount={onUnmount}
                 >
                     <MarkerF position={center} onClick={() => setVisible(true)}/>
-                    <MarkerF position={center2} />
+                    <MarkerF position={center2} onClick={() => setVisible(true)}/>
                 </GoogleMap>
 
             </div>
             <Modal
+                className="mainModalArea"
                 scroll
-                
                 blur
                 closeButton
                 aria-labelledby="modal-title"
                 aria-describedby="modal-description"
                 {...bindings}
-            >
+                >
                 <Modal.Header className="modalTitle">
                 <Text id="modal-title" size={18}>
                     Nombre Del Centro
                 </Text>
                 </Modal.Header>
                 <Modal.Body className="modalContainer">
-                   
+                <div uk-slider="center: true">
+
+                <div className="uk-position-relative uk-visible-toggle uk-dark" tabIndex={-1}>
+
+                    <ul className="uk-slider-items uk-child-width-1-2@s uk-child-width-1-4@l uk-child-width-1-4@m uk-grid">
+                        <li>
+                            <div className="uk-panel">
+                                <div className="box-container">
+                                <div className="card">
+                                        <div className="social">
+                                        <a href="" className="uk-icon-link" uk-icon="twitter" ></a>
+                                        <a href="" className="uk-icon-link" uk-icon="facebook"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="instagram"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="behance"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="linkedin"></a>
+                                        </div>
+                                        <div
+                                            className="pic"><img src="https://instagram.fcgk6-1.fna.fbcdn.net/vp/b97a5a48c4db220f58e53fd7712b4c4d/5D095A20/t51.2885-15/e35/51588782_245542016328142_2909652747855401869_n.jpg?_nc_ht=instagram.fcgk6-1.fna.fbcdn.net" alt="profile-picture" title="Someone Famous" />
+                                        </div>
+                                    <div className="content">
+                                        <h3 className="title">Dimas Aji</h3><span>Graphic Designer</span>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="uk-panel">
+                                <div className="box-container">
+                                <div className="card">
+                                        <div className="social">
+                                        <a href="" className="uk-icon-link" uk-icon="twitter" ></a>
+                                        <a href="" className="uk-icon-link" uk-icon="facebook"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="instagram"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="behance"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="linkedin"></a>
+                                        </div>
+                                        <div
+                                            className="pic"><img src="https://instagram.fcgk6-1.fna.fbcdn.net/vp/b97a5a48c4db220f58e53fd7712b4c4d/5D095A20/t51.2885-15/e35/51588782_245542016328142_2909652747855401869_n.jpg?_nc_ht=instagram.fcgk6-1.fna.fbcdn.net" alt="profile-picture" title="Someone Famous" />
+                                        </div>
+                                    <div className="content">
+                                        <h3 className="title">Dimas Aji</h3><span>Graphic Designer</span>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="uk-panel">
+                                <div className="box-container">
+                                <div className="card">
+                                        <div className="social">
+                                        <a href="" className="uk-icon-link" uk-icon="twitter" ></a>
+                                        <a href="" className="uk-icon-link" uk-icon="facebook"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="instagram"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="behance"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="linkedin"></a>
+                                        </div>
+                                        <div
+                                            className="pic"><img src="https://instagram.fcgk6-1.fna.fbcdn.net/vp/b97a5a48c4db220f58e53fd7712b4c4d/5D095A20/t51.2885-15/e35/51588782_245542016328142_2909652747855401869_n.jpg?_nc_ht=instagram.fcgk6-1.fna.fbcdn.net" alt="profile-picture" title="Someone Famous" />
+                                        </div>
+                                    <div className="content">
+                                        <h3 className="title">Dimas Aji</h3><span>Graphic Designer</span>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="uk-panel">
+                                <div className="box-container">
+                                <div className="card">
+                                        <div className="social">
+                                        <a href="" className="uk-icon-link" uk-icon="twitter" ></a>
+                                        <a href="" className="uk-icon-link" uk-icon="facebook"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="instagram"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="behance"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="linkedin"></a>
+                                        </div>
+                                        <div
+                                            className="pic"><img src="https://instagram.fcgk6-1.fna.fbcdn.net/vp/b97a5a48c4db220f58e53fd7712b4c4d/5D095A20/t51.2885-15/e35/51588782_245542016328142_2909652747855401869_n.jpg?_nc_ht=instagram.fcgk6-1.fna.fbcdn.net" alt="profile-picture" title="Someone Famous" />
+                                        </div>
+                                    <div className="content">
+                                        <h3 className="title">Dimas Aji</h3><span>Graphic Designer</span>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="uk-panel">
+                                <div className="box-container">
+                                <div className="card">
+                                        <div className="social">
+                                        <a href="" className="uk-icon-link" uk-icon="twitter" ></a>
+                                        <a href="" className="uk-icon-link" uk-icon="facebook"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="instagram"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="behance"></a>
+                                        <a href="" className="uk-icon-link" uk-icon="linkedin"></a>
+                                        </div>
+                                        <div
+                                            className="pic"><img src="https://instagram.fcgk6-1.fna.fbcdn.net/vp/b97a5a48c4db220f58e53fd7712b4c4d/5D095A20/t51.2885-15/e35/51588782_245542016328142_2909652747855401869_n.jpg?_nc_ht=instagram.fcgk6-1.fna.fbcdn.net" alt="profile-picture" title="Someone Famous" />
+                                        </div>
+                                    <div className="content">
+                                        <h3 className="title">Dimas Aji</h3><span>Graphic Designer</span>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+
+                    <a className="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                    <a className="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+
+                </div>
+
+                <ul className="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
+
+                </div>
                 </Modal.Body>
                 <Modal.Footer>
                 <Button flat auto color="error" onPress={() => setVisible(false)}>
