bfa80399bc3b2d8b8f7da558ac818558b940ef50
Luna <vultines@gmail.com>
2023-04-27 19:19:36 -0400

    Carousel Implemented

--- centros_i_web/src/pages/index.tsx
+++ centros_i_web/src/pages/index.tsx
@@ -40,8 +40,6 @@ export default function Home() {
           </div>
           
         </div>
-       
-    
 
       </div>
 
@@ -75,8 +73,47 @@ export default function Home() {
           </div>
         </div>
       </div>
-      
+
+      <div className="sliderContainer">
+        <div className="carousel">
+          <div className="carousel-inner">
+            <input className="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden checked></input>
+            <div className="carousel-item">
+              <img src="/images/kidsGroup.jpg"></img>
+            </div>
+
+            <input className="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden checked></input>
+            <div className="carousel-item">
+              <img src="/images/logo.png"></img>
+            </div>
+
+            <input className="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden checked></input>
+            <div className="carousel-item">
+              <img src="/images/mainPic.png"></img>
+            </div>
+
+            <label htmlFor="carousel-3" className="carousel-control prev control-2">‹</label>
+            <label htmlFor="carousel-2" className="carousel-control next control-1">›</label>
+            <label htmlFor="carousel-1" className="carousel-control prev control-2">‹</label>
+            <label htmlFor="carousel-3" className="carousel-control next control-2">›</label>
+            <label htmlFor="carousel-2" className="carousel-control prev control-3">‹</label>
+            <label htmlFor="carousel-1" className="carousel-control next control-3">›</label>
+            <ol className="carousel-indicators">
+              <li>
+                  <label htmlFor="carousel-1" className="carousel-bullet">•</label>
+              </li>
+              <li>
+                  <label htmlFor="carousel-2" className="carousel-bullet">•</label>
+              </li>
+              <li>
+                  <label htmlFor="carousel-3" className="carousel-bullet">•</label>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
 
     </div>
+    
   )
 }
